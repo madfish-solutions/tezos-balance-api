c633bb73e217696b9ac9641c3a524e630a3e4b28
serh11p <git@serh11p.com>
2021-08-12 12:30:35 +0300

    Update API Schema

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,12 +1,7 @@
 import { loadBalance } from "./pool";
 
-export async function fetchAllBalances(account: string, assetSlugs: string[]) {
-  const balances: Record<string, string> = {};
-  await Promise.all(
-    assetSlugs.map(async (assetSlug) => {
-      const balance = await loadBalance(account, assetSlug);
-      balances[assetSlug] = balance;
-    })
+export function fetchAllBalances(account: string, assetSlugs: string[]) {
+  return Promise.all(
+    assetSlugs.map((assetSlug) => loadBalance(account, assetSlug))
   );
-  return balances;
 }
