8aa98dfad6e07d9658b4527deedbefb1d0392c4f
serh11p <git@serh11p.com>
2021-09-02 18:31:34 +0300

    Fix prodErrorHandler

--- src/web/helpers.ts
+++ src/web/helpers.ts
@@ -10,10 +10,11 @@ export const prodErrorHandler: express.ErrorRequestHandler = (
   const code = err.code || err.status || 500;
   const codeMessage = http.STATUS_CODES[code];
 
-  res.statusCode = code;
-  res.end(
-    code === 500 && process.env.NODE_ENV === "production"
-      ? codeMessage
-      : (err.length && err) || err.message || codeMessage
-  );
+  res
+    .send(
+      code === 500 && process.env.NODE_ENV === "production"
+        ? codeMessage
+        : (err.length && err) || err.message || codeMessage
+    )
+    .status(code);
 };
